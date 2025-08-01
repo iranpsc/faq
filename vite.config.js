454a5278baf5c32ca885aeb9dd7eb1cbbd20bd66
abbass <abbas.ajorlou1371@gmail.com>
2025-08-01 15:04:57 +0330

    Dark/Light theme change bug fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -10,6 +9,5 @@ export default defineConfig({
             refresh: true,
         }),
         vue(),
-        tailwindcss(),
     ],
 });
