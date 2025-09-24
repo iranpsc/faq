7f31adbf53a569b97adb493fe156a30f64da052e
abbass <abbas.ajorlou1371@gmail.com>
2025-09-24 08:55:47 +0330

    - Small bug fix made"

--- config/cors.php
+++ config/cors.php
@@ -25,8 +25,8 @@ return [
         'http://127.0.0.1:8000',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
-        'http://localhost:3001',
-        'http://127.0.0.1:3001'
+        'http://localhost:3005',
+        'http://127.0.0.1:3005'
     ],
 
     'allowed_origins_patterns' => [],
